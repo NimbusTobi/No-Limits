bbd9e98a03e32cd760fbec29ea0ae904e796db69
Tobi O <tobmakesapps@gmail.com>
2021-11-17 01:57:19 -0500

    Added Images to SRC

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
 	return (
 		<Layout pageTitle="N.O. Limits - Introduction">
 			<p>I started my lifelong martial arts journey with Taekwondo at age five. After many years of training and fighting, I was able to earn my black belt and the opportunity to fight in top national and international competitions. My training quickly progressed and led me to my position as a professional mixed martial artist. I attribute my background as an Eagle scout and the many trials and tribulations I’ve faced in the wilderness with crafting the self-awareness and fighting spirit I maintain.<br></br><br></br>Through my experiences I have been tutored and coached by the best in the game, and it's been an honor to learn and hone their skills. It’s my passion to share this knowledge in my unique training system with others, to help take take abilities to the next level.</p>
-		</Layout>
+			<StaticImage
+	        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+	        src="../images/AboveTheClouds.jpg"
+	      />
+	    </Layout>
 	)
 }
 
